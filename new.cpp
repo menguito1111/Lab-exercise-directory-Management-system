3cbef851abda6808fd372e1105f79593afaf8b30
piso wifi <benzcarllozada123@gmail.com>
2024-08-20 21:23:28 +0800

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+void displayFiles(){
+     int choice;
+    cout << "1. To display list of Files" << endl;
+    cout << "2. To create new directory" << endl;
+    cout << "3. To change the working directory" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter a number: ";
+    cin >> choice;
+
+    switch(choice){
+        case 1:
+        cout << "1. List of files" << endl;
+        cout << "2. List of extension files" << endl;
+        cout << "3. list of name wise" << endl;
+        break;
+
+    }
+    
+}
